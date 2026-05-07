02e4e3522e240ecb574c38ade2d4d2c674a25a30
vietlt <luutheviet2016@gmail.com>
2026-05-07 22:26:48 +0700

    fix: move main class to io.finpilot package + suppress thymeleaf warning

---
+++ src/main/java/io/finpilot/FinpilotApplication.java
@@ -0,0 +1,13 @@
+package io.finpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinpilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinpilotApplication.class, args);
+	}
+
+}
